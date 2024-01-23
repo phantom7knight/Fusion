5af577d5ab1c3c118ad62fa1cae5ce26a593650f
phantom7k <mail2rohitolety@gmail.com>
2024-01-22 23:46:48 -0500

    WIP: Cube app setup

--- Fusion/Applications/Init/Init.cpp
+++ Fusion/Applications/Init/Init.cpp
@@ -2,31 +2,82 @@
 
 #include <nvrhi/nvrhi.h>
 #include <nvrhi/utils.h>
-#include "../../Core/Engine/ShaderFactory.h"
 
-bool InitApp::Init()
+#include "../../Core/Engine/CommonRenderPasses.h"
+#include "../../Core/Engine/TextureCache.h"
+#include "../../Core/App/DeviceManager.h"
+
+bool InitApp::InitAppShaderSetup(std::shared_ptr<donut::engine::ShaderFactory> aShaderFactory)
 {
-	std::filesystem::path appShaderPath = donut::app::GetDirectoryWithExecutable() / "../../../Assets/Shaders/Applications/Init/Generated";
+	mTriangle.mVertexShader = aShaderFactory->CreateShader("Triangle.hlsl", "main_vs", nullptr, nvrhi::ShaderType::Vertex);
+	mTriangle.mPixelShader = aShaderFactory->CreateShader("Triangle.hlsl", "main_ps", nullptr, nvrhi::ShaderType::Pixel);
+	
+	mCube.mVertexShader = aShaderFactory->CreateShader("Cube.hlsl", "main_vs", nullptr, nvrhi::ShaderType::Vertex);
+	mCube.mPixelShader = aShaderFactory->CreateShader("Cube.hlsl", "main_ps", nullptr, nvrhi::ShaderType::Pixel);
 
-	auto nativeFS = std::make_shared<donut::vfs::NativeFileSystem>();
-	donut::engine::ShaderFactory shaderFactory(GetDevice(), nativeFS, appShaderPath);
-
-	mVertexShader = shaderFactory.CreateShader("Triangle.hlsl", "main_vs", nullptr, nvrhi::ShaderType::Vertex);
-	mPixelShader = shaderFactory.CreateShader("Triangle.hlsl", "main_ps", nullptr, nvrhi::ShaderType::Pixel);
-
-	if (!mVertexShader || !mPixelShader)
+	if (!mTriangle.mVertexShader || !mTriangle.mPixelShader ||
+		!mCube.mVertexShader || !mCube.mPixelShader)
 	{
 		return false;
 	}
 
+	return true;
+}
+
+bool InitApp::Init()
+{
+	const std::filesystem::path baseShaderPath = donut::app::GetDirectoryWithExecutable() / "../../../Assets/Shaders/";
+	std::filesystem::path appShaderPath = baseShaderPath / "Applications/Init/Generated";
+	std::filesystem::path commonShaderPath = baseShaderPath / "Common";
+
+	std::shared_ptr<donut::vfs::RootFileSystem> rootFS = std::make_shared<donut::vfs::RootFileSystem>();
+	rootFS->mount("/shaders/Init", appShaderPath);
+	rootFS->mount("/shaders/app", commonShaderPath);
+
+	std::shared_ptr<donut::engine::ShaderFactory> shaderFactory = std::make_shared<donut::engine::ShaderFactory>(GetDevice(), rootFS, "/shaders/Init");
+
+	if (!InitAppShaderSetup(shaderFactory))
+		return false;
+
+	// todo_rt; testing
+	
+	//std::shared_ptr<donut::vfs::RootFileSystem> rootFS = std::make_shared<donut::vfs::RootFileSystem>();
+	//rootFS->mount("/shaders/donut", baseShaderPath);
+
 	mCommandList = GetDevice()->createCommandList();
+
+	/*mCube.mConstantBuffer = GetDevice()->createBuffer(nvrhi::utils::CreateStaticConstantBufferDesc
+	(sizeof(locInitHelpers::ConstantBufferEntry) * locInitHelpers::cNumViews, "Cube ConstantBuffer")
+		.setInitialState(nvrhi::ResourceStates::ConstantBuffer)
+		.setKeepInitialState(true));
+
+	nvrhi::VertexAttributeDesc attributes[] = {
+			nvrhi::VertexAttributeDesc()
+				.setName("POSITION")
+				.setFormat(nvrhi::Format::RGB32_FLOAT)
+				.setOffset(0)
+				.setBufferIndex(0)
+				.setElementStride(sizeof(locInitHelpers::Vertex)),
+			nvrhi::VertexAttributeDesc()
+				.setName("UV")
+				.setFormat(nvrhi::Format::RG32_FLOAT)
+				.setOffset(0)
+				.setBufferIndex(1)
+				.setElementStride(sizeof(locInitHelpers::Vertex)),
+	};
+
+	mCube.mInputLayout = GetDevice()->createInputLayout(attributes, uint32_t(std::size(attributes)), mCube.mVertexShader);
+
+	donut::engine::CommonRenderPasses cmnRenderPasses(GetDevice(), shaderFactory);
+	donut::engine::TextureCache textureCache(GetDevice(), nativeFS, nullptr);*/
+
 
 	return true;
 }
 
 void InitApp::BackBufferResizing()
 {
-	mGraphicsPipeline = nullptr;
+	mTriangle.mGraphicsPipeline = nullptr;
 }
 
 void InitApp::Animate(float fElapsedTimeSeconds)
@@ -37,15 +88,15 @@ void InitApp::Animate(float fElapsedTimeSeconds)
 
 void InitApp::Render(nvrhi::IFramebuffer* framebuffer)
 {
-	if (!mGraphicsPipeline)
+	if (!mTriangle.mGraphicsPipeline)
 	{
 		nvrhi::GraphicsPipelineDesc psoDesc;
-		psoDesc.VS = mVertexShader;
-		psoDesc.PS = mPixelShader;
+		psoDesc.VS = mTriangle.mVertexShader;
+		psoDesc.PS = mTriangle.mPixelShader;
 		psoDesc.primType = nvrhi::PrimitiveType::TriangleList;
 		psoDesc.renderState.depthStencilState.depthTestEnable = false;
 
-		mGraphicsPipeline = GetDevice()->createGraphicsPipeline(psoDesc, framebuffer);
+		mTriangle.mGraphicsPipeline = GetDevice()->createGraphicsPipeline(psoDesc, framebuffer);
 	}
 
 	mCommandList->open();
@@ -53,7 +104,7 @@ void InitApp::Render(nvrhi::IFramebuffer* framebuffer)
 	nvrhi::utils::ClearColorAttachment(mCommandList, framebuffer, 0, nvrhi::Color(0.f));
 
 	nvrhi::GraphicsState state;
-	state.pipeline = mGraphicsPipeline;
+	state.pipeline = mTriangle.mGraphicsPipeline;
 	state.framebuffer = framebuffer;
 	state.viewport.addViewportAndScissorRect(framebuffer->getFramebufferInfo().getViewport());
 
--- Fusion/Applications/Init/Init.h
+++ Fusion/Applications/Init/Init.h
@@ -1,15 +1,107 @@
 #pragma once
 
 #include "../../Core/App/ApplicationBase.h"
+#include "../../Core/Utilities/Math/math.h"
+#include "../../Core/Engine/ShaderFactory.h"
 
-//using namespace donut;
+namespace locInitHelpers
+{
+	struct Vertex
+	{
+		dm::float3 position;
+		dm::float2 uv;
+	};
+
+	static const Vertex gVertices[] = {
+		{ {-0.5f,  0.5f, -0.5f}, {0.0f, 0.0f} }, // front face
+		{ { 0.5f, -0.5f, -0.5f}, {1.0f, 1.0f} },
+		{ {-0.5f, -0.5f, -0.5f}, {0.0f, 1.0f} },
+		{ { 0.5f,  0.5f, -0.5f}, {1.0f, 0.0f} },
+
+		{ { 0.5f, -0.5f, -0.5f}, {0.0f, 1.0f} }, // right side face
+		{ { 0.5f,  0.5f,  0.5f}, {1.0f, 0.0f} },
+		{ { 0.5f, -0.5f,  0.5f}, {1.0f, 1.0f} },
+		{ { 0.5f,  0.5f, -0.5f}, {0.0f, 0.0f} },
+
+		{ {-0.5f,  0.5f,  0.5f}, {0.0f, 0.0f} }, // left side face
+		{ {-0.5f, -0.5f, -0.5f}, {1.0f, 1.0f} },
+		{ {-0.5f, -0.5f,  0.5f}, {0.0f, 1.0f} },
+		{ {-0.5f,  0.5f, -0.5f}, {1.0f, 0.0f} },
+
+		{ { 0.5f,  0.5f,  0.5f}, {0.0f, 0.0f} }, // back face
+		{ {-0.5f, -0.5f,  0.5f}, {1.0f, 1.0f} },
+		{ { 0.5f, -0.5f,  0.5f}, {0.0f, 1.0f} },
+		{ {-0.5f,  0.5f,  0.5f}, {1.0f, 0.0f} },
+
+		{ {-0.5f,  0.5f, -0.5f}, {0.0f, 1.0f} }, // top face
+		{ { 0.5f,  0.5f,  0.5f}, {1.0f, 0.0f} },
+		{ { 0.5f,  0.5f, -0.5f}, {1.0f, 1.0f} },
+		{ {-0.5f,  0.5f,  0.5f}, {0.0f, 0.0f} },
+
+		{ { 0.5f, -0.5f,  0.5f}, {1.0f, 1.0f} }, // bottom face
+		{ {-0.5f, -0.5f, -0.5f}, {0.0f, 0.0f} },
+		{ { 0.5f, -0.5f, -0.5f}, {1.0f, 0.0f} },
+		{ {-0.5f, -0.5f,  0.5f}, {0.0f, 1.0f} },
+	};
+
+	static const uint32_t gIndices[] = {
+		 0,  1,  2,   0,  3,  1, // front face
+		 4,  5,  6,   4,  7,  5, // left face
+		 8,  9, 10,   8, 11,  9, // right face
+		12, 13, 14,  12, 15, 13, // back face
+		16, 17, 18,  16, 19, 17, // top face
+		20, 21, 22,  20, 23, 21, // bottom face
+	};
+
+	constexpr uint32_t cNumViews = 4;
+
+	static const dm::float3 gRotationAxes[cNumViews] = {
+		dm::float3(1.f, 0.f, 0.f),
+		dm::float3(0.f, 1.f, 0.f),
+		dm::float3(0.f, 0.f, 1.f),
+		dm::float3(1.f, 1.f, 1.f),
+	};
+
+	// This example uses a single large constant buffer with multiple views to draw multiple versions of the same model.
+	// The alignment and size of partially bound constant buffers must be a multiple of 256 bytes,
+	// so define a struct that represents one constant buffer entry or slice for one draw call.
+	struct ConstantBufferEntry
+	{
+		dm::float4x4 viewProjMatrix;
+		float padding[16 * 3];
+	};
+
+	static_assert(sizeof(ConstantBufferEntry) == nvrhi::c_ConstantBufferOffsetSizeAlignment, "sizeof(ConstantBufferEntry) must be 256 bytes");
+}
 
 class InitApp : public donut::app::IRenderPass
 {
 private:
-	nvrhi::ShaderHandle mVertexShader;
-	nvrhi::ShaderHandle mPixelShader;
-	nvrhi::GraphicsPipelineHandle mGraphicsPipeline;
+
+	bool InitAppShaderSetup(std::shared_ptr<donut::engine::ShaderFactory> aShaderFactory);
+
+	struct Triangle
+	{
+		nvrhi::ShaderHandle mVertexShader;
+		nvrhi::ShaderHandle mPixelShader;
+		nvrhi::GraphicsPipelineHandle mGraphicsPipeline;
+	}mTriangle;
+
+	struct Cube
+	{
+		nvrhi::ShaderHandle mVertexShader;
+		nvrhi::ShaderHandle mPixelShader;
+		nvrhi::BufferHandle mConstantBuffer;
+		nvrhi::BufferHandle mVertexBuffer;
+		nvrhi::BufferHandle mIndexBuffer;
+		nvrhi::TextureHandle mTexture;
+		nvrhi::InputLayoutHandle mInputLayout;
+		nvrhi::BindingLayoutHandle mBindingLayout;
+		nvrhi::BindingSetHandle m_BindingSets[locInitHelpers::cNumViews];
+		nvrhi::GraphicsPipelineHandle mGraphicsPipeline;
+		float mRotation = 0.f;
+	}mCube;
+
 	nvrhi::CommandListHandle mCommandList;
 
 public:
